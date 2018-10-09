5626b4a6a24388b5cbb910eaf209cacc1c9d1491
Lucas Sindra <lucas.sindra@sap.com>
2018-10-09 11:07:17 -0300

    Separated functions into classes

---
+++ functions/src/routes/challenges.ts
@@ -0,0 +1,53 @@
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as _cors from 'cors';
+
+import { functionsConfig } from '../functions-config';
+
+export class Challenges {    
+    constructor(db: FirebaseFirestore.Firestore) {
+        this.challengeAPI = express();
+        const cors = _cors;
+        const CorsOptions = {
+            origin: functionsConfig.whitelist,
+            optionsSuccessStatus: 200
+        }
+        
+        this.challengeAPI.use(cors(CorsOptions));
+        this.challengeAPI.use(express.json());
+        this.challengeAPI.disable('etag');
+
+        const challengeCollection = db.collection('challenges');
+
+        //Get all challenges
+        this.challengeAPI.get('/', (req, res) => {
+            challengeCollection.get().then(snapshot => {
+                snapshot.docs.map(doc => res.send(doc.data()));
+            }).catch(error => {
+                console.log("Error getting document:", error);
+            })
+        });
+
+        //Get challenge by UID
+        this.challengeAPI.get('/:uid', (req, res) => {
+            const uid = req.params['uid'];
+
+            challengeCollection.doc(uid).get().then(function(challenge) {
+                if (challenge.exists) {
+                    res.send(challenge.data())
+                } else {
+                    console.log("No such challenge!");
+                }
+            }).catch(error => {
+                console.log("Error getting document:", error);
+            });
+        });
+    }
+
+    db;
+    challengeAPI;
+
+    toFunction() {
+        return functions.https.onRequest(this.challengeAPI);
+    }
+}

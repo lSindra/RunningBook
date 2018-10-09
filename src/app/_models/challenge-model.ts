e1d2b3f4c5f2a02da49a30d0b3afe5cc53fb0f91
Lucas Sindra <lucas.sindra@sap.com>
2018-10-09 11:27:18 -0300

    Adicionei challenge service

---
+++ src/app/_models/challenge-model.ts
@@ -0,0 +1,9 @@
+export class ChallengeModel {
+    title: string;
+    createdAt: Date;
+    description: string;
+    level: number;
+    reward: number;
+    participants: [];
+    uid: string;
+}
---
+++ src/app/_services/challenge.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AppConfigService } from '../config/app-config.service';
+import { ChallengeModel } from '../_models/challenge-model';
+
+@Injectable()
+export class ChallengeService {
+  constructor(private http: HttpClient) {}
+  private apiServer = AppConfigService.settings.apiServer.metadata;
+  
+  getChallengeByUID(uid: string): Observable<ChallengeModel> {
+    const url = this.apiServer + 'challengeAPI/';
+    return this.http.get<ChallengeModel>(url + uid);
+  }
+
+  getChallenges(): Observable<ChallengeModel[]> {
+    const url = this.apiServer + 'challengeAPI/';
+    return this.http.get<ChallengeModel[]>(url);
+  }
+}

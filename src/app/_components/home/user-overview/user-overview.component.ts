50bd82811dfdad32c08b8996c4ce672ef34908ec
Lucas Sindra <lucas.sindra@sap.com>
2018-10-10 00:51:40 -0300

    adicionado efeito de hover

--- src/app/_components/home/user-overview/user-overview.component.ts
+++ src/app/_components/home/user-overview/user-overview.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit, Directive } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-user-overview',
   templateUrl: './user-overview.component.html',
   styleUrls: ['./user-overview.component.scss']
 })
-export class UserOverviewComponent implements OnInit {
+export class UserOverviewComponent {
 
-  constructor() { }
+  constructor(public element: ElementRef<HTMLElement>) {}
 
-  ngOnInit() {
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.element.nativeElement.firstElementChild.classList.add('hovering');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.element.nativeElement.firstElementChild.classList.remove('hovering');
   }
 
 }

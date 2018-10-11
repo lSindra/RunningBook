faba72b87dcea1bb93e2eda49652afbfed627253
Lucas Sindra <lucas.sindra@sap.com>
2018-10-10 21:54:42 -0300

    Feed extend arrumado

--- src/app/_components/home/feed/feed.component.ts
+++ src/app/_components/home/feed/feed.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
-
+export class FeedComponent {
   constructor() { }
 
-  ngOnInit() {
+  @Output()
+  onReadMore: EventEmitter<any> = new EventEmitter()
+  
+  readMore() {
+    console.log("read");
+    //delete button
+
+    //expand
+    this.onReadMore.emit();
   }
 
 }
--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() { }
+export class HomeComponent {
+  constructor(
+    private renderer: Renderer2
+  ) { }
 
-  ngOnInit() {
+  @ViewChild('feedTile', { read: ElementRef })
+  feedTile:ElementRef;
+
+  readMore() {
+    let element = this.feedTile.nativeElement;
+    console.log(element);
+
+    this.removeReadMoreButton(element);
+    this.expand(element);
   }
 
+  removeReadMoreButton(element) {
+    const parent = element.firstChild.firstChild.firstChild;
+    const child = element.firstChild.firstChild.firstChild.lastChild;
+    this.renderer.removeChild(parent, child)
+  }
+  
+  expand(element) {
+    this.renderer.removeClass(element, "mat-grid-tile");
+    this.renderer.setStyle(element, 'position', 'absolute');
+    this.renderer.setStyle(element, 'display', 'table');
+    
+  }
 }
